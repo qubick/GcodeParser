ba35264faf9a23df16f016b42619f191296fa8d1
qubick <JeeeunKim@JeeeunKims-MacBook-Air.local>
2015-01-22 21:03:28 -0700

    got interpolated

--- parser.js
+++ parser.js
@@ -3,9 +3,9 @@ var fs 				= require('fs');
 //var lineReader	= require('line-by-line');
 
 
-var filename = "tian-layer5.gcode" 
+var filename = "tian.gcode" 
 var cnt = 0;
-var rDist = .5; //suppose regular distance is .5
+var rDist = 1; //suppose regular distance is .5
 
 var data;
 var prevX;
@@ -13,17 +13,7 @@ var prevY;
 
 function dist(x1,y1,x2,y2) {
 
-	return Math.sqrt((x2-x1)^2 + (y2-y1)^2)
-}
-
-function posX(x1,y1,x2,y2,a,b){
-
-	return (y1*(b^2+x2^2+y2^2) - y2*(a^2-x1^2-y1^2))/2*(x2*y1-x1*y2)
-}
-
-function posY(x1,y1,x2,y2,a,b){
-
-	return (x2*(x1^2-y1^2-a^2) - x1*(x2^2-y2^2+b^2))/2*(y1*x2-y2*x1)
+	return Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2))
 }
 
 function parser(fname, res) {
@@ -32,23 +22,21 @@ function parser(fname, res) {
 			throw err;
 		else {
 			var line = data.toString().split('\n');	
-			//console.log(gcode); //line == array of all lines
 			
 			//line.forEach(function(entry){ //not for each -> loop
 			//var tok = entry.split(' '); //take one line
 			var tok = line[cnt++].split(' ');
-//console.log(line[cnt]);
 			if(tok[0] === 'G1'){
 				prevX = tok[1];
 				prevY = tok[2];
 				
 				if(prevX[0] == 'X' && prevY[0] == 'Y'){
-					prevX = prevX.replace('X', '');
-					prevY = prevY.replace('Y', '');
+					prevX = parseFloat(prevX.replace('X', ''));
+					prevY = parseFloat(prevY.replace('Y', ''));
 					//write to file	
 				} 
 			} //get first line
-//console.log(line.length);
+			
 			for(var i=1; i<line.length; i++){				
 				tok = line[i].split(' '); //next line
 				
@@ -56,35 +44,47 @@ function parser(fname, res) {
 					var nextX = tok[1];
 					var nextY = tok[2];
 					if(nextX[0] == 'X' && nextY[0] == 'Y'){
-						nextX = nextX.replace('X', '');
-						nextY = nextY.replace('Y', '');
-					
-//***** 1st: interpolate with constant distance
-						var eDist = dist(prevX, prevY, nextX, nextY);
-						while (eDist > .5){
-							var interX = posX(prevX, prevY, nextX, nextY, rDist, eDist)
-							var interY = posY(prevX, prevY, nextX, nextY, rDist, eDist)
+						nextX = parseFloat(nextX.replace('X', ''));
+						nextY = parseFloat(nextY.replace('Y', ''));
 
-							prevX = interX;
-							prevY = interY;
-			
-							eDist = dist(prevX, prevY, nextX, nextY);
+						// interpolate with constant distance
+						var eDist = dist(prevX, prevY, nextX, nextY);
+						var theta = Math.atan((nextY-prevY)/(nextX-prevX));
+console.log('got next line from gcode file');
+console.log(prevX, prevY, nextX, nextY);
+
+						while (eDist > rDist){
+							if(prevX < nextX)
+								prevX = prevX + rDist*Math.cos(theta);
+							else if(prevX > nextX)
+								prevX = prevX - rDist*Math.cos(theta);
+
+							if(prevY < nextY)
+								prevY = prevY + rDist*Math.sin(theta);
+							else if(prevY > nextY)
+								prevY = prevY - rDist*Math.sin(theta);
+console.log('prevX: ', prevX, 'prevY: ', prevY);
+console.log('eDist: ', eDist, 'rDist: ', rDist);
+							//eDist = dist(prevX, prevY, nextX, nextY);
+							eDist -= rDist;
+							if(eDist < rDist){
+								prevX = nextX; prevY = nextY;
+							}
+							var newLine = prevX + '\t' + prevY + '\n';
+								fs.appendFile("xy.txt", newLine, function(err){
+								if(err) console.log(err);
+							})
 						}
+					//prevX = nextX; prevY = nextY;
+					
 					} else {
 						i++
-						console.log("reached here");
+						//console.log("reached here");
 					}
-				}
-//****** 2nd: interpolate with mid porints 
-
-			var newLine = interX + '\t' + interY + '\n';
-				//write file
-				fs.appendFile("xy.txt", newLine, function(err){
-					if(err) console.log(err);
-				})	
+				} //end of if	
 			} //end of for loop	
-		} //end of else
-	}); //end of fs.read()
-}
+		}
+	}) //end of fs.read()
+} //end of main
 
 parser(filename, data);
