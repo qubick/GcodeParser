1bcbeb29e68cd1ad0a2ac504bcaccaa4c7eea905
qubick <qubick.kim@gmail.com>
2015-01-24 13:22:56 -0700

    js style

--- parser.js
+++ parser.js
@@ -1,13 +1,13 @@
-var fs 				= require('fs');
-var validator 		= require('validator');
+var fs 				= require('fs'),
+	 validator 		= require('validator')
 //var vic			= require('victor');
 //var lineReader	= require('line-by-line');
 
 
 var inFile		 	= "tian-layer5.gcode" 
 var outFile		 	= "output.txt"
-var rDist 			= 1; //const: regular distance for interpolation btw two position
-var first			= 0;
+var rDist 			= 1 //const: regular distance for interpolation btw two position
+var first			= 0
 
 function dist(x1,y1,x2,y2) {
 
@@ -17,73 +17,73 @@ function dist(x1,y1,x2,y2) {
 function parser(fname) {
 	fs.readFile(fname, function(err, data){
 		if (err) 
-			throw err;
+			throw err
 		else {
-			var line = data.toString().split('\n');	
+			var line = data.toString().split('\n')	
 			
 			//line.forEach(function(entry){ //not for each -> loop
 			//var tok = entry.split(' '); //take one line
 			while(1){
-				var tok = line[first].split(' '); //get first line
+				var tok = line[first].split(' ') //get first line
 				if(tok[0] == 'G1'){
-					var prevX = tok[1];
-					var prevY = tok[2];
+					var prevX = tok[1]
+					var prevY = tok[2]
 					if(prevX[0] == 'X' && prevY[0] == 'Y'){
-						prevX = parseFloat(prevX.replace('X', ''));
-						prevY = parseFloat(prevY.replace('Y', ''));
+						prevX = parseFloat(prevX.replace('X', ''))
+						prevY = parseFloat(prevY.replace('Y', ''))
 					} 
 				}
 				if (validator.isFloat(prevX) && validator.isFloat(prevY)) 
-					break; //got first pos then break while
+					break //got first pos then break while
 				else
-					first++;
+					first++
 			}
 			
 			
 			for(var i=1; i<line.length; i++){				
-				tok = line[i].split(' '); //next line
+				tok = line[i].split(' ') //next line
 				
 				if(tok[0].toString() === 'G1'){
-					var nextX = tok[1];
-					var nextY = tok[2];
+					var nextX = tok[1]
+					var nextY = tok[2]
 					if(nextX[0] == 'X' && nextY[0] == 'Y'){
-						nextX = parseFloat(nextX.replace('X', ''));
-						nextY = parseFloat(nextY.replace('Y', ''));
+						nextX = parseFloat(nextX.replace('X', ''))
+						nextY = parseFloat(nextY.replace('Y', ''))
 console.log('got next line from gcode file: ', nextX, nextY);
 console.log(prevX, prevY);
 						// interpolate with constant distance
-						var eDist = dist(prevX, prevY, nextX, nextY);
+						var eDist = dist(prevX, prevY, nextX, nextY)
 console.log(eDist);
-						var theta = Math.atan(Math.abs(nextY-prevY)/Math.abs(nextX-prevX));
+						var theta = Math.atan(Math.abs(nextY-prevY)/Math.abs(nextX-prevX))
 
 						while (eDist > rDist){
 							if(prevX < nextX)
-								prevX = prevX + rDist * Math.cos(theta);
+								prevX = prevX + rDist * Math.cos(theta)
 							else if(prevX > nextX)
-								prevX = prevX - rDist * Math.cos(theta);
+								prevX = prevX - rDist * Math.cos(theta)
 
 							if(prevY < nextY)
-								prevY = prevY + rDist * Math.sin(theta);
+								prevY = prevY + rDist * Math.sin(theta)
 							else if(prevY > nextY)
-								prevY = prevY - rDist * Math.sin(theta);
+								prevY = prevY - rDist * Math.sin(theta)
 
 console.log('X: ', prevX, 'Y: ', prevY);
 //console.log('Distance btw current pos to next pos(points from gcode: ', eDist);
 
 							//eDist -= rDist;
-							eDist = dist(prevX, prevY, nextX, nextY);
+							eDist = dist(prevX, prevY, nextX, nextY)
 
-							newLine = prevX + ',' + prevY + '\n';
+							newLine = prevX + ',' + prevY + '\n'
 							fs.appendFile(outFile, newLine, function(err){
-								if(err) console.log(err);
+								if(err) console.log(err)
 							})
 						}
 						if(eDist < rDist){
-							prevX = nextX; prevY = nextY;
+							prevX = nextX; prevY = nextY
 							
-							newLine = prevX + ',' + prevY + '\n';
+							newLine = prevX + ',' + prevY + '\n'
 							fs.appendFile(outFile, newLine, function(err){
-								if(err) console.log(err);
+								if(err) console.log(err)
 							})
 						}	
 					} else {
@@ -96,4 +96,4 @@ console.log('X: ', prevX, 'Y: ', prevY);
 	}) //end of fs.read()
 } //end of main
 
-parser(inFile);
+parser(inFile)
